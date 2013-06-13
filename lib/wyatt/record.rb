216742f2dd0220533b0548780dd282c910b78b5f
James Christie <james.aaron.christie@gmail.com>
2013-06-13 13:31:12 -0500

    Expand ORM functionality to provide more field tools
    - Add support for a list of Ruby classes as types
    - Add support for human readable field label
    - Add support for a remote field type string

--- lib/wyatt/record.rb
+++ lib/wyatt/record.rb
@@ -4,24 +4,39 @@ module Wyatt
 
     class Record
 
-      def self.add_field_to_class(field_name, type)
+      def self.add_field_to_class(field_name, allowed_types, remote_type)
+
         class_eval <<-RUBY
+
           def #{field_name}
-            @field_name
+            @#{field_name}
           end
 
           def #{field_name}=(value)
-            raise_#{field_name}_type_exception if value.class != #{type}
-            @field_name = value 
+            unless #{field_name}_allowed_types.include?(value.class)            
+              raise_#{field_name}_type_exception
+            end
+
+            @#{field_name} = value 
+          end
+
+          def #{field_name}_allowed_types
+            #{allowed_types}
+          end
+
+          def #{field_name}_remote_type
+            "#{remote_type}"
           end
 
           private
 
           def raise_#{field_name}_type_exception
-            message = "#{field_name} only accepts an instance of #{type}"
+            message = "#{field_name} only accepts instances from #{allowed_types}"
             raise Wyatt::Exceptions::InvalidType.new(message)
           end
+
         RUBY
+
       end
 
     end
@@ -29,3 +44,5 @@ module Wyatt
   end
 
 end
+
+# support list of allowed types
